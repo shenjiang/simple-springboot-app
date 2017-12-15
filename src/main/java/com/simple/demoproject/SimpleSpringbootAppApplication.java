36482dd0bbec52221f8a1998f1c72a82450949be
Shen Jiang <jiangshen@xinyuancapital.com>
2017-12-15 16:52:39 +0800

    init

---
+++ src/main/java/com/simple/demoproject/SimpleSpringbootAppApplication.java
@@ -0,0 +1,12 @@
+package com.simple.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootAppApplication.class, args);
+	}
+}
